df8a5d4be48c902124eaef05b600f3f1b5e38ab7
DmytroNiehrii <Dmytro.Niehrii@playtech.com>
2017-09-27 17:08:07 +0300

    HT1 implementation

--- src/components/Article.js
+++ src/components/Article.js
@@ -10,15 +10,24 @@ class Article extends Component {
         article: PropTypes.shape({
             title: PropTypes.string.isRequired,
             text: PropTypes.string,
-            date: PropTypes.string.isRequired
+            date: PropTypes.string.isRequired,
+            comments: PropTypes.array,
         }).isRequired,
         isOpen: PropTypes.bool,
         onClick: PropTypes.func
     }
 
+    state = {
+        isCommentsExpanded: false
+    }
+
     render() {
         const {article, isOpen, onButtonClick} = this.props
-        const body = isOpen && <section>{article.text}</section>
+        const body = <section>{article.text}</section>
+        const comments = article.comments || [];
+        const commentItems = comments.map((comment, index)=>(<div key={comment.id}><b>{comment.user}: </b>{comment.text}</div>));
+
+
         return (
             <div>
                 <h2>
@@ -27,11 +36,24 @@ class Article extends Component {
                         {isOpen ? 'close' : 'open'}
                     </button>
                 </h2>
-                {body}
+                <div hidden={!isOpen}>
+                    {body}
+                    <button onClick={this.onCommentButtonClick}>{this.state.isCommentsExpanded ? 'Hide comments' : 'Show comments'}</button>
+                    <div hidden={!this.state.isCommentsExpanded}>
+                        {commentItems}
+                    </div>
+                </div>
+
                 <h3>creation date: {(new Date(article.date)).toDateString()}</h3>
             </div>
         )
     }
+
+    onCommentButtonClick = () => (
+        this.setState({
+            isCommentsExpanded: !this.state.isCommentsExpanded
+        })
+    );
 }
 
 
